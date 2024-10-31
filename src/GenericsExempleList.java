8be613cf8adeb6e8c1641c363e5a5294d08d5035
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-10-30 22:35:04 -0300

    Initial commit

---
+++ src/GenericsExempleList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsExempleList {
+
+    public static void main(String[] args) {
+
+    List listaSemGenerics = new ArrayList();
+    listaSemGenerics.add("Elemento 1");
+    listaSemGenerics.add(10);
+
+    }
+}
