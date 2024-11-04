738da7988842fc36982710b04ef3e52cc6f6b438
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-03 22:51:19 -0300

    Testes

--- src/OperacoesBasicas/ConjuntoConvidados.java
+++ src/OperacoesBasicas/ConjuntoConvidados.java
@@ -24,6 +24,29 @@ public class ConjuntoConvidados {
                 break;
             }
         }
+        convidadosSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidadosSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadosSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1236);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+
     }
 
 }
