2ceb49a13f8f2aa68fc6e36db88de33634b61d6c
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-11-05 20:09:28 -0300

    Classe produto

---
+++ src/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,9 @@
+package Ordenacao;
+
+import java.util.Set;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+
+}
---
+++ src/Ordenacao/Produto.java
@@ -0,0 +1,59 @@
+package Ordenacao;
+
+import java.util.Objects;
+
+public class Produto {
+    private long codigo;
+
+    private String nome;
+
+    private double preco;
+
+    private int quantidade;
+
+    public Produto(long codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + codigo +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
